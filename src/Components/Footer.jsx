40a626a05e2fe5148878a9650bf9b5fedf408455
Aditya Ghogale <60639350+adityaghogale@users.noreply.github.com>
2021-06-11 15:14:23 +0530

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const Footer=(props)=>{
+    return(
+        <div>
+            <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="mailto:adityaghogale07@gmail.com">Mail me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tel:9325856458">Call Me</a>
+        </li>
+      </ul>
+    </nav>
+        </div>
+    )
+}
+export default Footer;
